4d63c7ba8deb81278ed2ba5078535310511fbc4d
Asif Azad <asifazad799@gmail.com>
2021-09-25 20:18:25 +0530

    completed Sps-Website

--- video.js
+++ video.js
@@ -1,10 +1,70 @@
 const spsimg = document.querySelector(".spsimg");
+const spsvideo = document.querySelector(".spsvideo");
+
 
 const videoplacer = () =>{
     spsimg.classList.add("hidden");
     spsvideo.classList.remove("hidden");
     spsvideo.setAttribute(
         "src",
-        
-    )
-}
+        "https://www.youtube.com/embed/I9QGnNvrmoY?autoplay=1"
+    );
+};
+
+spsimg.addEventListener("click",videoplacer);
+
+const spsimg2 = document.querySelector(".spsimg-2");
+const spsvideo2 = document.querySelector(".spsvideo-2");
+
+const videoplacer2 = () =>{
+    spsimg2.classList.add("hidden");
+    spsvideo2.classList.remove("hidden");
+    spsvideo2.setAttribute(
+        "src",
+        "https://www.youtube.com/embed/ckub1RSlhic?autoplay=1" 
+    );
+};
+
+spsimg2.addEventListener("click",videoplacer2);
+
+const spsimg3 = document.querySelector(".spsimg-3");
+const spsvideo3 = document.querySelector(".spsvideo-3");
+
+const videoplacer3 = () =>{
+    spsimg3.classList.add("hidden");
+    spsvideo3.classList.remove("hidden");
+    spsvideo3.setAttribute(
+        "src",
+        "https://www.youtube.com/embed/xkmM6h32lnM?autoplay=1" 
+    );
+};
+
+spsimg3.addEventListener("click",videoplacer3);
+
+const spsimg4 = document.querySelector(".spsimg-4");
+const spsvideo4 = document.querySelector(".spsvideo-4");
+
+const videoplacer4 = () =>{
+    spsimg4.classList.add("hidden");
+    spsvideo4.classList.remove("hidden");
+    spsvideo4.setAttribute(
+        "src",
+        "https://www.youtube.com/embed/vhG3wKGp2mc?autoplay=1" 
+    );
+};
+
+spsimg4.addEventListener("click",videoplacer4);
+
+const spsimg5 = document.querySelector(".spsimg-5");
+const spsvideo5 = document.querySelector(".spsvideo-5");
+
+const videoplacer5 = () =>{
+    spsimg5.classList.add("hidden");
+    spsvideo5.classList.remove("hidden");
+    spsvideo5.setAttribute(
+        "src",
+        "https://www.youtube.com/embed/m_hx0Ebg5Yo?autoplay=1"
+    );
+};
+
+spsimg5.addEventListener("click",videoplacer5);
